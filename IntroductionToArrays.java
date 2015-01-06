3a9097678166218f15b623fff37275cf3b669c5a
Verndari <doschmail@gmail.com>
2015-01-06 22:09:29 +0100

    Tutorials

---
+++ IntroductionToArrays.java
@@ -0,0 +1,23 @@
+package thenewboston;
+
+public class IntroductionToArrays {
+	public static void main (String [] args) {
+		
+		int bucky[] = {2,4,5,7,9};					// Variant 2 Define bucky var with a String of Numbers
+		
+		System.out.println(bucky[2]);				// Print out a Number from the array to the console. (Array list is count from 0 to end so 2 in this case is equal to 5 in the array)
+		//array initialiser
+	}
+	
+/*	int bucky[] = new int[10];						// Variant 1 Define bucky var with a fixed array
+	
+	bucky[0] = 87;									// After the array is defined to 10
+	bucky[1] = 543;									// You can create 0 to 9 new single arrays with
+	bucky[9] = 65;									// different numbers
+	
+	System.out.println(bucky[1]);					// Print out one of the fixed array we defined above eg. [9] would be printed with "65"
+	
+}*/
+	
+	
+}
