dc2a40aabbb04801727ace3d99515c5c6e0d8c5a
Daniel <doschmail@gmail.com>
2015-01-07 04:19:36 +0100

    Update ConditionalOperators.java

--- ConditionalOperators.java
+++ ConditionalOperators.java
@@ -3,7 +3,7 @@ package thenewboston;
 public class ConditionalOperators {
 	public static void main (String[] args) {
 		
-		int age = 45;														// Define a new Age var
+		int age = 45;								// Define a new Age var
 		System.out.println(age > 50 ? "You are old" : "You are young");		// Print with a ">" and Yes : No Operator
 																			
 	}
