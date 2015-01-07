535ca739473519b9eef9e795f1ed75c4df09b1df
Daniel <doschmail@gmail.com>
2015-01-07 04:20:31 +0100

    Update DoWhileLoops.java

--- DoWhileLoops.java
+++ DoWhileLoops.java
@@ -3,12 +3,12 @@ package thenewboston;
 public class DoWhileLoops {
 	public static void main (String [] args) {
 		
-		int counter = 0;					// Define a counter starts at 0
+		int counter = 0;			// Define a counter starts at 0
 		
 		do{
 			System.out.println(counter);	// Print out the counter to the console
-			counter++;						// Increment the counter by +1
-		} while(counter <= 10);				// Run the do statement until it hits 10 
+			counter++;			// Increment the counter by +1
+		} while(counter <= 10);			// Run the do statement until it hits 10 
 		
 	}
 }
