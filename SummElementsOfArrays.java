29d64c4d6d1aec3567de3c1cb3a19850312f4664
Daniel <doschmail@gmail.com>
2015-01-07 04:22:10 +0100

    Update SummElementsOfArrays.java

--- SummElementsOfArrays.java
+++ SummElementsOfArrays.java
@@ -4,11 +4,11 @@ public class SummElementsOfArrays {
 
 	public static void main(String[] args) {
 		
-		int bucky[]	= {21,16,86,21,3};								// Define a new bucky integer with an array initialiser
-		int sum = 0;												// Define a sum integer
+		int bucky[]	= {21,16,86,21,3};				// Define a new bucky integer with an array initialiser
+		int sum = 0;							// Define a sum integer
 		
-		for(int counter=0;counter<bucky.length;counter++) {			// Create a For counter Loop with the length from the array
-			sum += bucky[counter];									// Loops through the whole array and creates the sum of it
+		for(int counter=0;counter<bucky.length;counter++) {		// Create a For counter Loop with the length from the array
+			sum += bucky[counter];					// Loops through the whole array and creates the sum of it
 		}
 		
 		System.out.println("The sum of these numbers is " +sum);	// Print out the sum of the array (eg. 147)
