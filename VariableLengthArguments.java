cb6fa187df10c0e7e75756b01c62edf69f3a0f51
Daniel <doschmail@gmail.com>
2015-01-07 08:43:37 +0100

    Update VariableLengthArguments.java

--- VariableLengthArguments.java
+++ VariableLengthArguments.java
@@ -8,10 +8,10 @@ public class VariableLengthArguments {
 	}
 	
 	public static int average(int...numbers){			// Create a Method with a var that doesn't know which number it should contains
-		int total = 0;									// Create an total Integer
-		for(int x:numbers)								// Create a for loop with integer x that contains numbers
-			total += x;									// Add total to x
+		int total = 0;						// Create an total Integer
+		for(int x:numbers)					// Create a for loop with integer x that contains numbers
+			total += x;					// Add total to x
 		
-		return total/numbers.length;					// return total and the exact amount of numbers
+		return total/numbers.length;				// return total and the exact amount of numbers
 	}
 }
