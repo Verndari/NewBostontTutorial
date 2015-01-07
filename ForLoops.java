7191ef05fe7eb1b5da6293189795c0de156b2d9f
Daniel <doschmail@gmail.com>
2015-01-07 04:20:51 +0100

    Update ForLoops.java

--- ForLoops.java
+++ ForLoops.java
@@ -4,7 +4,7 @@ public class ForLoops {
 	public static void main (String [] args){
 		
 		for(int counter=1;counter<=10;counter++) {	// For method to count from 1 to 10
-			System.out.println(counter);			// Print the result at the console
+			System.out.println(counter);		// Print the result at the console
 		}
 		
 	}
